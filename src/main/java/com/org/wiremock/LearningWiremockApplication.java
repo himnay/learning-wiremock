ef45439a061a71b0e8040781e9ac412d49261616
Himansu Nayak <himansuxnayak@gmail.com>
2022-03-06 06:57:58 +0000

    initial draft

---
+++ src/main/java/com/org/wiremock/LearningWiremockApplication.java
@@ -0,0 +1,13 @@
+package com.org.wiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningWiremockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningWiremockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/wiremock/LearningWiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.wiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningWiremockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
